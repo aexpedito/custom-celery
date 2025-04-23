2b622b3960d6d6c0d40ccbaeb3e92c5db71f57cb
aexpedito <aexpedito@hotmail.com>
2025-04-23 17:34:21 -0300

    changes in celery

--- src/tasks/celery.py
+++ src/tasks/celery.py
@@ -7,5 +7,15 @@ app.conf.update(
     result_expires = 3600,
 )
 
+# Pegar a mensagem do 
 if __name__ == '__main__':
+
+    # Cria kafka consumer
+
+    # Inicializa o Celery app
     app.start()
+
+    # Para cada mensagem do topico Kafka
+    # for message in consumer:
+    # app.add.delay(message.value)
+    print("Celery worker started.")
--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -1,5 +1,9 @@
 from .celery import app
+from time import sleep
 
 @app.task
 def add(x, y):
+    sleep(5)
+    """Add two numbers."""
+    print(f"Adding {x} + {y}")
     return x + y
